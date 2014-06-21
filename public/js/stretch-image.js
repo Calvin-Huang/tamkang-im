fe8dcfd9db9ee1be24ff0d38d4e1fc72012890f5
Calvin-Huang <ts01364362@gmail.com>
2014-06-22 07:24:46 +0900

    fit news detail attach image width

--- public/js/stretch-image.js
+++ public/js/stretch-image.js
@@ -1,11 +1,11 @@
 $('.attach-image').click(function(handler){
-    var template = '<img class="img-polaroid" src="' + $(this).data('src') + '">';
+    var template = '<img class="img-responsive" src="' + $(this).data('src') + '">';
     $(this).html(template);
 });
 
 $('#stretch-all').click(function(handler){
     $('.attach-image').each(function(i){
-        var template = '<img class="img-polaroid" src="' + $(this).data('src') + '">';
+        var template = '<img class="img-responsive" src="' + $(this).data('src') + '">';
         $(this).html(template);
     });
 });
