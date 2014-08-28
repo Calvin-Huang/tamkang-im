d15ad68fed5c4915fb457e2183b634afeb88d810
Calvin-Huang <ts01364362@gmail.com>
2014-08-28 09:26:10 +0900

    disable exception and error showing

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -65,8 +65,10 @@ return array(
         ),
     ),
     'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
+
+        // disable showing exceptions or errors
+        'display_not_found_reason' => false,
+        'display_exceptions'       => false,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
